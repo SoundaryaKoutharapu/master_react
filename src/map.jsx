fe6df5a394923b8482fdf378547b2a570d7591d4
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-05-17 20:08:28 +0530

    properlist

--- src/map.jsx
+++ src/map.jsx
@@ -1,28 +1,36 @@
 // map function
 
-const Books = 
+const books = 
+[
 {
     author: 'Paulo Coelho',
     title: 'The Alchemist',
-    img:'https://m.media-amazon.com/images/I/41e+TR4xPDL._SX328_BO1,204,203,200_.jpg',
+    img:'https://m.media-amazon.com/images/I/41e+TR4xPDL._SX328_BO1,204,203,200_.jpg'
+},
 
+{
     author: ' Sudha Murty ',
     title: "Grandma's Bag of Stories",
     img:' https://m.media-amazon.com/images/I/61yB0UFlM3L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg' 
-
 }
+]
 
-const names = ['john','hima', 'susan'];
+//const names = ['john','hima', 'susan'];
 
-const newNames = names.map((name) => 
+/*  const newNames = names.map((name) => 
 {
    return <h1>{name}</h1>
-});
+}); */
 
 
 const Map = () => {
 // return <div>{newNames}</div>
 return <div>
+    {books.map((book) =>{
+        console.log(book);
+        return <h1>{book.title}</h1>
+
+    })}
 </div>
 }
 export default Map
