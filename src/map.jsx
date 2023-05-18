152b586b81dd552e7321515c02697cb57261c553
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-05-18 21:27:01 +0530

    id's added

--- src/map.jsx
+++ src/map.jsx
@@ -5,13 +5,15 @@ const books =
 {
     author: 'Paulo Coelho',
     title: 'The Alchemist',
-    img:'https://m.media-amazon.com/images/I/41e+TR4xPDL._SX328_BO1,204,203,200_.jpg'
+    img:'https://m.media-amazon.com/images/I/41e+TR4xPDL._SX328_BO1,204,203,200_.jpg',
+    id:'1'
 },
 
 {
     author: ' Sudha Murty ',
     title: "Grandma's Bag of Stories",
-    img:' https://m.media-amazon.com/images/I/61yB0UFlM3L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg' 
+    img:' https://m.media-amazon.com/images/I/61yB0UFlM3L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg' ,
+    id:'2'
 }
 ]
 
